ef95eed9d795b2e45a142f2c8b2caa0c8953b27f
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2023-06-13 10:38:05 -0300

    add: html and css files

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, "recipes/home.html")
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
-
-
-def contato(request):
-    return HttpResponse("CONTATO")
+    return render(request, "recipes/pages/home.html")
